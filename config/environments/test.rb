59da8ad79a399fe562d633dfebc54a5b2cb34d9f
Jed Schneider <jed.schneider@gmail.com>
2015-02-13 15:45:05 -0500

    add some config to see if we can get travis to tell us whats going on

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # will this give us more info on travis?
+  config.assets.raise_runtime_errors = true
+  config.assets.debug = true
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
